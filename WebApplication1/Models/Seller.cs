2e0265948e6d9fa3c103f149e10153797d63dbfb
vikumpramoda <66731366+vikumpramoda@users.noreply.github.com>
2022-07-26 08:06:45 +0530

    SellerCRUD

---
+++ WebApplication1/Models/Seller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Seller
+    {
+        [Key]
+        public int SellerId { get; set; } = 0;
+        public string Name { get; set; } = "";
+        public string Email { get; set; } 
+        public int Age { get; set; } = 0;
+    }
+}
